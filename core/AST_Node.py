936818fa16827ccc85d06c4db38792f558597b0f
Mark Joseph Octavo <31540335+MAX25M@users.noreply.github.com>
2026-02-17 23:05:56 +0800

    Add AST_Node.py with Program and GateNode classes
    Signed-off-by: Mark Joseph Octavo  <31540335+MAX25M@users.noreply.github.com>

---
+++ core/AST_Node.py
@@ -0,0 +1,15 @@
+class Program:
+    def __init__(self, statements):
+        self.statements = statements
+
+class GateNode:
+    def __init__(self, name, target, angle=None):
+        self.name = name
+        self.target = target
+        self.angle = angle  # None for fixed gates like H or CNOT
+
+class MeasurementNode:
+    def __init__(self, qubit, classical_reg):
+        self.qubit = qubit
+        self.classical_reg = classical_reg
+      
