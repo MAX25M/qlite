2e9211f9dc4f9b9647a0ac8ec14508a26198f591
Mark Joseph Octavo <31540335+MAX25M@users.noreply.github.com>
2026-02-18 19:40:05 +0800

    commit

--- core/parser.py
+++ core/parser.py
@@ -1,9 +1,27 @@
 import ply.yacc as yacc
-from .lexer import tokens  # Import tokens from our Lexer
+from .lexer import tokens
+import math
 
+# --- AST Node Classes ---
+class Program:
+    def __init__(self, statements):
+        self.statements = statements
+
+class GateNode:
+    def __init__(self, name, target, angle=None):
+        self.name = name
+        self.target = target
+        self.angle = angle
+
+class MeasurementNode:
+    def __init__(self, target, bit_name):
+        self.target = target
+        self.bit_name = bit_name
+
+# --- Grammar Rules ---
 def p_program(p):
     'program : statement_list'
-    p[0] = Program(p[1])
+    p[0] = p[1]
 
 def p_statement_list(p):
     '''statement_list : statement
@@ -13,27 +31,22 @@ def p_statement_list(p):
     else:
         p[0] = p[1] + [p[2]]
 
-# Handle Qubit Declaration: qubit q[2];
 def p_statement_declaration(p):
     'statement : QUBIT ID LBRACKET INTEGER RBRACKET SEMICOLON'
-    p[0] = ('DECLARE', p[2], p[4])
+    p[0] = {'type': 'DECLARE', 'id': p[2], 'size': p[4]}
 
-# Handle Fixed Gates: H q[0];
 def p_statement_fixed_gate(p):
     'statement : GATE_FIXED qarg SEMICOLON'
     p[0] = GateNode(p[1], p[2])
 
-# Handle Rotational Gates: RX(3.14) q[0];
 def p_statement_rot_gate(p):
     'statement : GATE_ROT LPAREN expression RPAREN qarg SEMICOLON'
     p[0] = GateNode(p[1], p[5], angle=p[3])
 
-# Handle Measurement: q[0] => c0;
 def p_statement_measure(p):
     'statement : qarg ARROW ID SEMICOLON'
     p[0] = MeasurementNode(p[1], p[3])
 
-# Simple Math Expressions for Angles
 def p_expression_binop(p):
     '''expression : expression PLUS expression
                   | expression MINUS expression
@@ -55,7 +68,6 @@ def p_expression_number(p):
 
 def p_expression_pi(p):
     'expression : PI'
-    import math
     p[0] = math.pi
 
 def p_qarg(p):
@@ -63,7 +75,16 @@ def p_qarg(p):
     p[0] = f"{p[1]}[{p[3]}]"
 
 def p_error(p):
-    print(f"Syntax error at '{p.value}'")
+    if p:
+        print(f"Syntax error at '{p.value}'")
+    else:
+        print("Syntax error at EOF")
 
-# Build the parser
-parser = yacc.yacc()
+# --- Parser Entry Point ---
+class Parser:
+    def __init__(self):
+        # Build the yacc parser using the global functions in this module
+        self.parser = yacc.yacc()
+
+    def parse(self, data):
+        return self.parser.parse(data)
